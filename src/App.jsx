fc5ccb06af298915922a486850410d2f6d0b7b9c
chethan <chethanchethu99339@gmail.com>
2024-08-29 07:53:34 +0530

    simple update the project in design

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import { useState } from "react";
 
-import "./App.css";
-
-function Btt({ bttColor, setColor }) {
+function Btt({bgColor, title, setColor}) {
   return (
-    <>
-      <button
-        className="text-[white] p-2 rounded-xl mx-4 min-w-[5rem] font-bold"
-        style={{
-          backgroundColor: bttColor,
-        }}
-        onClick={() => setColor(bttColor)}
-      >{bttColor}</button>
-    </>
+    <button
+      className="font-bold min-w-[5rem] p-2 rounded-xl text-[white] mx-2 drop-shadow-lg"
+      style={{ backgroundColor: bgColor }}
+      onClick={()=> setColor(bgColor)}
+    >{title}</button>
   );
 }
 
@@ -20,23 +14,26 @@ function App() {
   const [color, setColor] = useState("lightBlue");
 
   return (
-    <div
-      className="w-screen h-screen duration-500 "
-      style={{
-        backgroundColor: color,
-      }}
-    >
-      <div className=" text-center bg-[white] p-3 flex flex-wrap justify-center fixed bottom-20 w-full  ">
-        <Btt bttColor='red' setColor={setColor} />
-        <Btt bttColor='blue' setColor={setColor} />
-        <Btt bttColor='yellow' setColor={setColor} />
-        <Btt bttColor='green' setColor={setColor} />
-        <Btt bttColor='pink' setColor={setColor} />
-        <Btt bttColor='orange' setColor={setColor} />
-        <Btt bttColor='purple' setColor={setColor} />
-        <Btt bttColor='gray' setColor={setColor} />
+    <>
+      <div
+        className="w-screen h-screen duration-300 flex justify-center items-end "
+        style={{ backgroundColor: color }}
+      >
+        <div className="bg-[white] p-3   flex flex-wrap justify-center items-center mb-20 rounded-md drop-shadow-md">
+
+          <Btt title="Red" bgColor="red" setColor={setColor} />
+          <Btt title="yellow" bgColor="yellow" setColor={setColor} />
+          <Btt title="green" bgColor="green" setColor={setColor} />
+          <Btt title="blue" bgColor="blue" setColor={setColor} />
+          <Btt title="pink" bgColor="pink" setColor={setColor} />
+          <Btt title="gray" bgColor="gray" setColor={setColor} />
+          <Btt title="purple" bgColor="purple" setColor={setColor} />
+          <Btt title="orange" bgColor="orange" setColor={setColor} />
+          
+
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
